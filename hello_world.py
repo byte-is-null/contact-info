45dc5ab914bfc12aae5aa07c0f4a737312f79387
Nullified <91416696+byte-is-null@users.noreply.github.com>
2023-01-07 22:38:23 +0800

    delayy
    slow af

--- hello_world.py
+++ hello_world.py
@@ -3,4 +3,5 @@ import itertools
 x = [150,145,154,154,157,40,167,157,162,154,144]
 for a,b,c in itertools.product(itertools.count(0),xrange(0,10),xrange(0,10)):
 	print a,b,c
+	#20ms delay between each character
 	if a > 20: break
