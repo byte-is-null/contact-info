fd408d7c16dafa8ddd875303d185a6e5d820e618
Nullified <91416696+byte-is-null@users.noreply.github.com>
2023-01-24 10:52:33 +0800

    clear as crystal

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,11 @@
 import itertools
 
+#randomly numbers
 x = [150,145,154,154,157,40,167,157,162,154,144]
+
+#decrypto
 for a,b,c in itertools.product(itertools.count(0),xrange(0,10),xrange(0,10)):
 	print a,b,c
-	#20ms delay between each character
+
+	#20ms delay between each character??
 	if a > 20: break
